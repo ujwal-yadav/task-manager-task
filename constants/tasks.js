7c35d867dc95c9e877bf28910a44ffe1011186cb
ujwal-yadav <ujwalofficial99@gmail.com>
2024-10-19 18:16:45 +0530

    feat: add mock tasks

---
+++ constants/tasks.js
@@ -0,0 +1,39 @@
+export const Tasks = [
+  {
+    id: 1,
+    title: 'Complete project proposal',
+    description: 'Draft and submit the project proposal by Friday.',
+    priority: 'High',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Review team performance',
+    description: 'Conduct quarterly performance reviews for team members.',
+    priority: 'Medium',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Update software dependencies',
+    description:
+      'Check and update all software dependencies to their latest versions.',
+    priority: 'Low',
+    completed: false,
+  },
+  {
+    id: 4,
+    title: 'Schedule team meeting',
+    description: 'Arrange a meeting to discuss project updates and deadlines.',
+    priority: 'Medium',
+    completed: true,
+  },
+  {
+    id: 5,
+    title: 'Prepare presentation for stakeholders',
+    description:
+      'Create and refine the presentation for the next stakeholder meeting.',
+    priority: 'High',
+    completed: false,
+  },
+];
