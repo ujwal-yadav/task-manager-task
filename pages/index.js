0ab2ab09c3c6c0f937316efff4e535644155dbcb
ujwal-yadav <ujwalofficial99@gmail.com>
2024-10-19 18:17:11 +0530

    feat: add task manager ui

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,168 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+import { useState } from 'react';
+import { PlusIcon } from 'lucide-react';
+import { Modal } from '@/components/Modal';
+import { TaskForm } from '@/components/TaskForm';
+import { motion } from 'framer-motion';
+import { TaskCard } from '@/components/TaskCard';
+import { Tasks } from '@/constants/tasks';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+export default function Home({ initialTasks }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tasks, setTasks] = useState(initialTasks);
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [searchText, setSearchText] = useState('');
+  const PriorityOrder = {
+    High: 1,
+    Medium: 2,
+    Low: 3,
+  };
 
-export default function Home() {
+  const addTask = (values) => {
+    const newTask = {
+      ...values,
+      id: Date.now(),
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+    setIsModalOpen(false);
+  };
+
+  const updateTask = (id, selectedTask) => {
+    setTasks(tasks.map((task) => (task.id === id ? selectedTask : task)));
+    setSelectedTask(null);
+    setIsModalOpen(false);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const filteredTasks = tasks
+    .filter(
+      (task) =>
+        task.title.toLowerCase().includes(searchText.toLowerCase()) ||
+        task.description.toLowerCase().includes(searchText.toLowerCase())
+    )
+    .sort((a, b) => PriorityOrder[a.priority] - PriorityOrder[b.priority]);
+
+  const openModal = (task = null) => {
+    setSelectedTask(task);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedTask(null);
+  };
+
+  const searchTask = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const completedTasks = filteredTasks.filter((task) => task.completed);
+  const activeTasks = filteredTasks.filter((task) => !task.completed);
+
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="bg-zinc-900 text-white min-h-screen">
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <TaskForm
+          selectedTask={selectedTask}
+          addTask={addTask}
+          updateTask={updateTask}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      </Modal>
+      <div className="container mx-auto lg:p-6 p-4 lg:w-[800px]">
+        <div className="flex justify-between items-center lg:mb-6 mb-4">
+          <h1 className="text-3xl font-semibold">Tasks</h1>
+          <button
+            onClick={() => openModal()}
+            className="inline-flex gap-1 p-2 px-3 font-medium bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <PlusIcon /> Add Task
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        <div className="lg:my-6 my-4">
+          <input
+            type="text"
+            placeholder="Search task"
+            value={searchText}
+            onChange={searchTask}
+            className="bg-zinc-700 w-full px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-white"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+        <div className="lg:space-y-6 space-y-4">
+          {activeTasks.map((task) => (
+            <motion.div
+              key={task.id}
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.3 }}
+            >
+              <TaskCard
+                task={task}
+                openModal={openModal}
+                deleteTask={deleteTask}
+                toggleComplete={toggleComplete}
+                isCompleted={false}
+              />
+            </motion.div>
+          ))}
+        </div>
+
+        {completedTasks.length > 0 ? (
+          <>
+            <h2 className="text-2xl font-semibold lg:mt-6 mt-4 lg:mb-4 mb-2">
+              Completed Tasks
+            </h2>
+            <div className="flex flex-col lg:gap-6 gap-4">
+              {completedTasks.map((task) => (
+                <motion.div
+                  key={task.id}
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <TaskCard
+                    task={task}
+                    openModal={openModal}
+                    deleteTask={deleteTask}
+                    toggleComplete={toggleComplete}
+                    isCompleted={true}
+                  />
+                </motion.div>
+              ))}
+            </div>
+          </>
+        ) : (
+          activeTasks.length === 0 && (
+            <>
+              <p className="text-center text-xl">No tasks</p>
+            </>
+          )
+        )}
+      </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const initialTasks = Tasks;
+
+  return {
+    props: {
+      initialTasks,
+    },
+  };
 }
